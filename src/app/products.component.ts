82eceb690a464926c385437c3daf93d1c4f9f2bc
Avinash <avikshirsagar4@gmail.com>
2021-01-26 12:32:32 +0530

    changes made to the project

---
+++ src/app/products.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-products',
+    templateUrl: './products.component.html'
+})
+
+export class ProductsComponent {
+    productName = 'A Book';
+    isDisabled = true;
+    products = ['A book', 'A tree'];
+    constructor() {
+        setTimeout(() => {
+            this.isDisabled = false;
+        }, 3000);
+    }
+    onAddProduct(){
+        this.products.push(this.productName);
+    }
+}
